0b2e8d395128cbdcec58a95e42f4d083b14dba93
Krishna Paikine <119587231+gutsdkrishna@users.noreply.github.com>
2024-08-20 18:16:19 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/Common/Header";
-import MainComponent from "../components/LandingPage/MainComponent";
 import Dashboard from "./Dashboard";
 
 function Home() {
