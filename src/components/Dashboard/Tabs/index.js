932d623ef6332818886d06a741ea00902209909f
Krishna Paikine <119587231+gutsdkrishna@users.noreply.github.com>
2024-08-20 19:22:31 +0530

    .

--- src/components/Dashboard/Tabs/index.js
+++ src/components/Dashboard/Tabs/index.js
@@ -9,7 +9,7 @@ import List from "../List";
 import Button from "../../Common/Button";
 
 export default function TabsComponent({ coins, setSearch }) {
-  const [value, setValue] = React.useState("grid");
+  const [value, setValue] = React.useState("list");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -29,34 +29,10 @@ export default function TabsComponent({ coins, setSearch }) {
     <TabContext value={value}>
       <div style={{ borderBottom: 1, borderColor: "divider" }}>
         <TabList onChange={handleChange} variant="fullWidth">
-          <Tab label="Grid" value="grid" sx={style} />
-          <Tab label="List" value="list" sx={style} />
+          
         </TabList>
       </div>
-      <TabPanel value="grid">
-        <div className="grid-flex">
-          {coins.length > 0 ? (
-            coins.map((coin, i) => (
-              <Grid coin={coin} key={i} delay={(i % 4) * 0.2} />
-            ))
-          ) : (
-            <div>
-              <h1 style={{ textAlign: "center" }}>
-                Sorry, Couldn't find the coin you're looking for 😞
-              </h1>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  margin: "2rem",
-                }}
-              >
-                <Button text="Clear Search" onClick={() => setSearch("")} />
-              </div>
-            </div>
-          )}
-        </div>
-      </TabPanel>
+      
       <TabPanel value="list">
         <table className="list-flex">
           {coins.length > 0 ? (
